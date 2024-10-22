4b4efac197601d74f4f27ca7a95fec4b779170c5
Alejandro Pastor <alepasblas77777@gmail.com>
2024-10-22 13:22:07 +0200

    first commit

---
+++ models/patient.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+let patientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 2,
+        maxlength: 50,
+    },
+    surname: {
+        type: String,
+        required: true,
+        maxlength: 2,
+        maxlength: 50,
+    },
+    birthDate: {
+        type: Date,
+        required: true,
+    },
+    address: {
+        type: String,
+        maxlength: 100,
+
+    },
+    insuranceNumber:{
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[a-zA-Z0-9]{9}$/,
+
+    }
+});
+
+let Patient = mongoose.model('patients', patientSchema);
+module.exports = Patient;
---
+++ models/record.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const consSchema = new mongoose.Schema({
+    date:{
+        type: Date,
+        required: true
+    },
+    physio:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'physio',
+        required: true,
+    },
+    diagnosis:{
+        type: String,
+        required:true,
+        minlength:10,
+        maxlength:500
+    },
+    treatment:{
+        type: String,
+        required:true
+    },
+    observations:{
+        type: String,
+        maxlength:500
+    }
+  });
+
+let recordSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'patient'
+    },
+    medicalRecord: {
+        type: String,
+        maxlength: 1000
+    },
+    appointments: [consSchema],
+
+});
+
+let Record = mongoose.model('records', recordSchema);
+module.exports = Record;
